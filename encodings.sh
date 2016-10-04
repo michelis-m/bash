5442e8a20b83e467961d2932a8765eba5bfde88a
michelis-m <michelis_m@yahoo.com>
2016-10-04 12:07:14 +0100

    Update encodings.sh

--- encodings.sh
+++ encodings.sh
@@ -1,86 +1,126 @@
 #!/bin/bash
 read -p "Insert connector name:" sn
 read -p "Insert version name:" vn
-read -p "Server name: (dev,int,prod)" svr
-a="leader.mpp.${svr}.emea.media.global.loc"
-read -p "Database name:" dbn
 read -p "Password for paraccel: " -s SSHPASS
 export SSHPASS
 
 pdw="pdw_${sn}_${vn}"
 canonical="canonical_${sn}_${sn}_${vn}"
 
-#Create the analyze compression commands
-echo "Creating analyze compression commands..."
-psql -h ${a} -p 5439 -U paraccel -w ${dbn} -c "
-select 'ANALYZE COMPRESSION '||table_schema||'.'||table_name||';' from information_schema.tables
-where table_schema IN ('${canonical}','${pdw}')
-and table_name not like '%_history'
-and table_type = 'BASE TABLE'" -t > analyze_comp.sql
+echo "Checking sort/dist keys and encodings..."
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "SELECT 
+BTRIM(current_database()) AS db_name 
+, BTRIM(n.nspname) AS schema_name 
+, BTRIM(name) AS tbl_name
+,cast(sum(a.attsortkeyord) as bool) 				hassortkey
+,cast(sum(cast(a.attisdistkey as INT )) as bool) 	hasdistkey
+,cast(sum(a.attencodingtype) as bool) 				hasencodtype
+FROM stv_tbl_perm t 
+JOIN pg_class c ON t.id = c.oid::BIGINT 
+JOIN pg_attribute a ON c.oid = a.attrelid 
+JOIN pg_namespace n ON c.relnamespace = n.oid 
+WHERE db_id <> 1 and schema_name in ('${pdw}', '${canonical}')
+GROUP BY 1,2,3
+HAVING sum(a.attsortkeyord) = 0 OR sum(cast(a.attisdistkey as INT )) = 0 
+OR sum(a.attencodingtype) = 0" > temp.txt
 
-#Run the analyze compression in all tables
-echo "Running analyze compression...(This might take a while)"
-psql -h ${a} -p 5439 -U paraccel -w ${dbn} -f analyze_comp.sql
+cat temp.txt
 
-#Moving the files locally
-sshpass -e scp paraccel@${a}:/tmp/*.ddl /home/mmiche01/MyScripts/analyzed/
-#Cleanup
-sshpass -e ssh paraccel@${a} rm /tmp/*.ddl
-
-#Get the table_names
-psql -h ${a} -p 5439 -U paraccel -w ${dbn} -c "
-select table_name from information_schema.tables
-where table_schema IN ('${canonical}','${pdw}')
-and table_name not like '%_history'
-and table_type = 'BASE TABLE'" -t > analyzed/${sn}.txt
-
-sed '/^\s*$/d' -i analyzed/${sn}.txt
-
-i=0
-j=0
-
-rm tmp1.sql
-
-for i in ${array[@]}
-do
-	echo $i
-	psql -h ${a} -p 5439 -U paraccel -w ${dbn} -c "select column_name from information_schema.columns
-	where table_schema IN ('${canonical}','${pdw}') and table_name = '${i}'" -t > tmp.txt
-	
-	sed '/^\s*$/d' -i tmp.txt
+read -r -p "Are you happy with this? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	echo "Good"
+else
+	echo "Not good. I will exit now..."
+	rm temp.txt
+	exit 0
+fi
 
 
-	#Read each column
-	while IFS='' read -r linec || [[ -n "$linec" ]]; do
-    	#echo "Column: $linec"
-    	#Storing column_names in an array
-    	array1[$j]=${linec}
+rm temp.txt
 
-    	#Reading the DDLs
-    	find /home/mmiche01/MyScripts/analyzed -name "*.${i}*" -exec cat {} \; | awk -v tb="${i}" -v myvar="$linec" '$0 ~ myvar {match($0, /encoding/); print "ALTER TABLE "tb" ALTER COLUMN"myvar" ENCODE " substr($0, RSTART + 9, RLENGTH +10);}' | sed 's/(//'| sed 's/)//'|sed 's/is .*//'|sed 's/vs\. .*//'|sed 's/$/\;/' >> tmp1.sql
-
-
-    	j=$((i+1))
-
-	done < tmp.txt
-
-done
-
-#to be changed. Will read the encoding custom formats
-psql -h ${a} -p 5439 -U paraccel -w ${dbn} -c "select * from dontdelete" -t > tmp_enc.sql
-#formatting
-sed '/^\s*$/d' -i tmp_enc.sql
-sed 's/| //' -i tmp_enc.sql
-sed 's/ \+/ /g' -i tmp_enc.sql
-sed 's/^[ \t]*//' -i tmp_enc.sql 
-
-awk 'NR==FNR && a[$1]=$2 {} NR>FNR { k=$6; if (k in a) print $1,$2,$3,$4,$5,k,$7,a[k]";"; else print $0}' tmp_enc.sql tmp1.sql > alters_${sn}.sql
-
-#padb_export current schemas
 echo "Running padb_export on server for pdw schema..."
 sshpass -e ssh paraccel@leader.mpp.dev.emea.media.global.loc padb_export pdw_dev -n ${pdw} -x -s -O -f ${pdw}.sql
 echo "Running padb_export on server for canonical schema..."
 sshpass -e ssh paraccel@leader.mpp.dev.emea.media.global.loc padb_export pdw_dev -n ${canonical} -x -s -O -f ${canonical}.sql
 
+echo "Copying the file pdw locally..."
+sshpass -e scp paraccel@leader.mpp.dev.emea.media.global.loc:/home/paraccel/${pdw}.sql /home/mmiche01/MyScripts
+echo "Copying the file canonical locally..."
+sshpass -e scp paraccel@leader.mpp.dev.emea.media.global.loc:/home/paraccel/${canonical}.sql /home/mmiche01/MyScripts
+
+echo "Removing generated files from server..."
+sshpass -e ssh paraccel@leader.mpp.dev.emea.media.global.loc rm ${pdw}.sql
+sshpass -e ssh paraccel@leader.mpp.dev.emea.media.global.loc rm ${canonical}.sql
+
+echo "Generating ACLs..."
+#_a group
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'ALTER TABLE '||table_schema||'.'||table_name||' OWNER TO :ga ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t > ${sn}_ACLs.sql
+
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT ALL ON '||table_schema||'.'||table_name||' TO GROUP :ga ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+#_w group
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT SELECT ON '||table_schema||'.'||table_name||' TO GROUP :gw ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT INSERT ON '||table_schema||'.'||table_name||' TO GROUP :gw ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT UPDATE ON '||table_schema||'.'||table_name||' TO GROUP :gw ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT DELETE ON '||table_schema||'.'||table_name||' TO GROUP :gw ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+#_r group
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT SELECT ON '||table_schema||'.'||table_name||' TO GROUP :gr ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+#_o group
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT SELECT ON '||table_schema||'.'||table_name||' TO GROUP :go ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+psql -h leader.mpp.dev.emea.media.global.loc -p 5439 -U paraccel -w pdw_dev -c "select 'GRANT REFERENCES ON '||table_schema||'.'||table_name||' TO GROUP :go ;' from information_schema.tables
+where table_schema IN ('${canonical}', '${pdw}');" -t >> ${sn}_ACLs.sql
+
+############################################# Deployment to INT ############################################
+read -p "Insert how many databases you want to deploy to:" yn
+read -p "Server name: (dev,int,prod)" svr
+a="leader.mpp.${svr}.emea.media.global.loc"
+
+for ((i=1; i <= ${yn} ; i++ ))
+do
+	read -p "Database name:" dbn
+	array[$i]=${dbn}
+done
+
+
+
+for i in ${array[@]}
+do
+	echo "Running for ${i} pdw schema..."
+	psql -h ${a} -p 5439 -U paraccel ${i} -f ${pdw}.sql -v ga=${i}_a -v gw=${i}_w -v gr=${i}_r -v go=${i}_o;
+
+	echo "Running for ${i} canonical schema..."
+	psql -h ${a} -p 5439 -U paraccel ${i} -f ${canonical}.sql -v ga=${i}_a -v gw=${i}_w -v gr=${i}_r -v go=${i}_o;
+
+	echo "Running for ${i} schema permissions..."
+	psql -h ${a} -p 5439 -U paraccel ${i} -c "ALTER SCHEMA ${pdw} OWNER TO ${i}_a ;
+	ALTER SCHEMA ${canonical} OWNER TO ${i}_a ;
+	GRANT ALL ON SCHEMA ${pdw} TO GROUP ${i}_a ;
+	GRANT ALL ON SCHEMA ${canonical} TO GROUP ${i}_a ;
+	GRANT USAGE ON SCHEMA ${pdw} TO GROUP ${i}_r ;
+	GRANT USAGE ON SCHEMA ${canonical} TO GROUP ${i}_r ;
+	GRANT USAGE ON SCHEMA ${pdw} TO GROUP ${i}_o ;
+	GRANT USAGE ON SCHEMA ${canonical} TO GROUP ${i}_o ;
+	GRANT USAGE ON SCHEMA ${pdw} TO GROUP ${i}_w ;
+	GRANT USAGE ON SCHEMA ${canonical} TO GROUP ${i}_w ;
+	"
+
+	echo "Running for ${i} ACLs..."
+	psql -h ${a} -p 5439 -U paraccel ${i} -f ${sn}_ACLs.sql -v ga=${i}_a -v gw=${i}_w -v gr=${i}_r -v go=${i}_o;
+done
 
 exit 0
